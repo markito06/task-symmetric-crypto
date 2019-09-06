5822e3070c59e9d92b6e34996042c8bd9a24db24
Marcos Alexandre V. Costa <marcos.avc@gmail.com>
2019-09-06 19:42:50 -0300

    descobre texto

--- simmetric-encryption/src/main/java/br/ufsc/ine/security/App.java
+++ simmetric-encryption/src/main/java/br/ufsc/ine/security/App.java
@@ -4,6 +4,22 @@ import java.util.Scanner;
 
 public class App {
 	public static void main(String[] args) {
+		//responderCifrarDecifrar();
+		responderDescobrirTexto();
+
+	}
+
+	private static void responderDescobrirTexto() {
+		String chaveCBC = "61db043f03148b11ac184af2c33a7659";
+		String ivAndTexto = "2f70627169811ad3732352be6ce82132c373d66f593332592ce1d4802296813932828bc"
+				+ "4511d32e6ff15ccb55fb3436c27d8856520f5f212d9a6efe1c8";
+		ProjetoAesEncontraTexto aesCbc = new ProjetoAesEncontraTexto("AES/CBC/PKCS5Padding", chaveCBC);
+		String decrypt = aesCbc.decrypt(ivAndTexto);
+		System.out.println(decrypt);
+		
+	}
+
+	private static void responderCifrarDecifrar() {
 		ProjetoAesEntraDados obj = new ProjetoAesEntraDados();
 
 		String paraCifrar;
@@ -19,7 +35,6 @@ public class App {
 			decifrada = obj.decrypt(cifrada);
 			System.out.println("Mensagem decifrada = " + decifrada);
 		}
-
 	}
 
 }
---
+++ simmetric-encryption/src/main/java/br/ufsc/ine/security/ProjetoAesEncontraTexto.java
@@ -0,0 +1,81 @@
+package br.ufsc.ine.security;
+
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+public class ProjetoAesEncontraTexto {
+
+	private Key aesKey;
+	private Cipher cipher;
+	private static Logger logger = Logger.getLogger(ProjetoAesEncontraTexto.class.getName());
+
+	public ProjetoAesEncontraTexto(String algAndMode, String key) {
+		logger.info("Configurando cifrador " + algAndMode);
+
+		try {
+			cipher = Cipher.getInstance(algAndMode);
+			byte[] encodedKey = Hex.decodeHex(key.toCharArray());
+			SecretKey originalKey = new SecretKeySpec(encodedKey, "AES");
+			this.aesKey = originalKey;
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | DecoderException e) {
+			logger.log(Level.SEVERE, null, e);
+		}
+
+		logger.info("Fim da configuração");
+
+	}
+
+	public String decrypt(String dec) {
+		try {
+			
+			IvParameterSpec  ivParameterSpec =  new IvParameterSpec(extractIv(dec));
+			cipher.init(Cipher.DECRYPT_MODE, aesKey, ivParameterSpec);
+			String decryptedString = new String(cipher.doFinal(extractTexto(dec)));
+			return decryptedString;
+
+		} catch (IllegalBlockSizeException | BadPaddingException |  InvalidKeyException
+				| InvalidAlgorithmParameterException e) {
+			logger.log(Level.SEVERE, null, e);
+		}
+		return null;
+	}
+
+	private byte[] extractTexto(String dec) {
+		String textPart = dec.substring(32);
+		byte[] text = {};
+		try {
+		text = Hex.decodeHex(textPart.toCharArray());
+		} catch (DecoderException e) {
+			logger.log(Level.SEVERE, null, e);
+		}
+		return text;
+	}
+
+	private byte[] extractIv(String dec) {
+ 		String ivPart = dec.substring(0,32);
+		System.out.println(ivPart);
+		byte[] iv = {}; 
+		try {
+			iv = Hex.decodeHex(ivPart.toCharArray());
+		} catch (DecoderException e) {
+			logger.log(Level.SEVERE, null, e);
+		}
+		return iv;
+	}
+
+}
